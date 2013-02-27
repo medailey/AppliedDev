693d37aace033d74e282a5f66dde43dc74d5ed2a
medailey <medaileyjr@gmail.com>
2013-02-27 18:56:39 -0500

    Create CreateCharacter.cs

---
+++ CreateCharacter.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class CreateCharacter
+    {
+       public void getCharacterName()
+        {
+            Console.SetCursorPosition(10, 10);
+            Console.WriteLine("Please Enter a Character Name:");
+        }
+    }
+}
